19912cffdb1127debfeede546cd1517952154f28
Etson <etsond@ufl.edu>
2024-06-11 15:44:42 -0400

    added student page

---
+++ scripts/students.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+console.log("We are connected");
+
+
+// fields
+const viewOptions = document.getElementById("view-options");
+const viewAll = document.getElementById("viewall");
+
+const productsTable = document.getElementById("productsTable");
+
+const addSingleStudent = document.getElementById("add-single-student");
+
+
+// buttons
+const addStudentBtn = document.getElementById("add-student-btn");
+
+
+window.onload = function () {
+
+}
