e6883983753a55cace03ef6447cb8d00d8c4eda4
Adama Darboe <adadadarboe@gmail.com>
2024-06-12 15:02:26 -0400

    stuff

--- scripts/students.js
+++ scripts/students.js
@@ -1,11 +1,13 @@
 "use strict";
 
 
+// we want to populate the drop dowm //
+
 console.log("We are connected");
 
 
 // fields
-const viewOptions = document.getElementById("view-options");
+
 const viewAll = document.getElementById("viewall");
 
 const productsTable = document.getElementById("productsTable");
@@ -18,5 +20,68 @@ const addStudentBtn = document.getElementById("add-student-btn");
 
 
 window.onload = function () {
+    let siteData = getSiteData();
+    console.log(siteData);
+    populateStudentOptions (siteData);
+}
+
+function populateStudentOptions (siteData) {
+    const students = siteData.students;
+    const viewOptions = document.getElementById("view-options");
+    viewOptions.innerHTML="";
+    // console.log(students);
+// default option
+const selectOneOption = document.createElement("option");
+selectOneOption.value = "";
+selectOneOption.text = "select One please";
+viewOptions.appendChild(selectOneOption);
+
+const viewAllOption = document.createElement("option");
+viewAllOption.value = "";
+viewAllOption.text = "View All";
+viewOptions.appendChild(viewAllOption);
+ 
+
+students.forEach(student =>{ 
+    const option = document.createElement("option");
+    option.value = student.studentName;
+    option.text = student.studentName;
+    viewOptions.appendChild (option);
+} )
+ }
+// function to display all of the studens 
+
+function displayAllStudents(siteData){
+    const students = siteData.students;
+    const studentList = document.getElementById("student-List");
+    studentList.innerHTML="";
+
+    students.forEach(student =>{
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        nameCell.innerHTML = student.studentName;
+        
+        const emailCell =document.createElement("td");
+        emailCell.innerHTML = student.studentEmail;
+        
+        const removeCell = document.createElement("td");
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = "X";
+        removeButton.classList.add("btn", "btn-danger");
+        removeButton.onclick = function(){
+            populateStudentOptions(getSiteData());
+            displayAllStudents(getSiteData());
+
+
+        }
+        removeCell.appendChild(removeButton);
+        row.appendChild(nameCell);
+        row.appendChild(emailCell);
+        row.appendChild(removeCell);
+
+        studentList.appendChild(row);
+
+    })
 
 }
+
